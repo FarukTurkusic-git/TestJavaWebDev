2c20773e3936ba681a30cb58d685be1d39c2ca2b
Faruk <Faruk@Faruk-PC>
2018-10-15 01:19:52 +0200

    ispravka komentara

--- CrunchifySpringMVCTutorial/src/com/crunchify/controller/CrunchifyHelloWorld.java
+++ CrunchifySpringMVCTutorial/src/com/crunchify/controller/CrunchifyHelloWorld.java
@@ -3,7 +3,7 @@ package com.crunchify.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-/* dodato radi rutiranja  */
+/* dodato radi rutiranja /welcome.jsp err: 404 */
 @Controller
 @RequestMapping(value="/")
 public class CrunchifyHelloWorld {
